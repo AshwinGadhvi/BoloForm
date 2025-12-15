4d362014c4728371a0b2cf8e7292a692c105074b
AshwinGadhvi <ashwingadhvi397@gmail.com>
2025-12-15 16:31:25 +0530

    adding some url.

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://boloform-tzcg.onrender.com/api"
 });
 
 // 🔐 Attach JWT automatically
